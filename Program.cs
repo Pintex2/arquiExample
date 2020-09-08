4c9f39c4354cad8f49ba4bd779fd839371ad78dc
Pintex2 <Andreumark@hotmail.com>
2020-09-07 23:56:05 -0400

    Arqui example

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArquiExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ officeWorker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArquiExample
+{
+     class officeWorker
+    {
+        public class OfficeWorker : Teacher, Person
+        {
+            private string degre;
+            public string position { get; set; }
+
+            public int moneyEarned { get; set; }
+
+            public OfficeWorker()
+            {
+                degre = "Master in administration public";
+                position = "administration";
+            }
+            public void extraHours()
+            {
+                moneyEarned = 5000;
+
+            }
+            public void positon()
+            {
+
+            }
+    }
+}
